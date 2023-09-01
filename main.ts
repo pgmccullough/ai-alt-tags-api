b2576a1c25050bb97f49213b7773993ed2f56bc4
Patrick Glendon McCullough <p@mccullo.ug>
2023-09-01 15:29:26 -0400

    headers.get('origin')

--- main.ts
+++ main.ts
@@ -8,8 +8,8 @@ const router = new Router();
 
 const verify = async (context: Context, next: () => Promise<unknown>) => {
   const headers: Headers = context.request.headers;
-  console.log("req from ",context.request.headers);
-  if(context.request.url.host==="ai-alt-tags.com") await next();
+  console.log("req from ",headers.get("origin"));
+  if(headers.get("origin")==="ai-alt-tags.com") await next();
   // check DB to see status of user
   if(!headers.get('AI-Alt-API-Key')||(Deno.env.get("TEMP_UUID")!==headers.get('AI-Alt-API-Key'))) return context.response.status = 401;
   await next();
