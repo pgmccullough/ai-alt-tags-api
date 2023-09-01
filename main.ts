9ce57cdbcf1bec925c928e341b93cd31fd5b0831
Patrick Glendon McCullough <p@mccullo.ug>
2023-09-01 15:33:48 -0400

    allow from dev

--- main.ts
+++ main.ts
@@ -9,6 +9,7 @@ const router = new Router();
 const verify = async (context: Context, next: () => Promise<unknown>) => {
   const headers: Headers = context.request.headers;
   if(headers.get("origin")==="https://ai-alt-tags.com") await next();
+  if(headers.get("origin")==="http://localhost:8000") await next();
   // check DB to see status of user
   if(!headers.get('AI-Alt-API-Key')||(Deno.env.get("TEMP_UUID")!==headers.get('AI-Alt-API-Key'))) return context.response.status = 401;
   await next();
